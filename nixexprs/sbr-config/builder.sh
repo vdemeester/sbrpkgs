1a6d5d398ffbaf2309a0b6d044f17948a74fdc91
Vincent Demeester <vincent@sbr.pm>
2016-09-21 22:42:56 +0200

    sbr-config: copy also non hidden files
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-config/builder.sh
+++ nixexprs/sbr-config/builder.sh
@@ -11,8 +11,11 @@ installPhase() {
     sbr-config-underscore-to-dot
     for file in .*; do
         if !(test "$file" == "." || test "$file" == ".."); then
-            cp -vR $file $out/nix-home/$file
+            cp -vR $file $out/nix-home/
         fi
+    done
+    for file in *; do
+	cp -vR $file $out/nix-home/
     done
 }
 
