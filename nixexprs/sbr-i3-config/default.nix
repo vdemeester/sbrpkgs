0d91942177ee5ab6818009eb50b396e19677822c
Vincent Demeester <vincent@sbr.pm>
2016-09-23 20:15:12 +0200

    Update i3 configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-i3-config/default.nix
+++ nixexprs/sbr-i3-config/default.nix
@@ -1,8 +1,8 @@
 { stdenv, system ? builtins.currentSystem, sbr-config }:
 
 sbr-config rec {
-  url = https://github.com/vdemeester/i3-config/archive/v1.1.tar.gz;
-  sha256 = "e31ade34c1db1401a33f83fa9a79dfd90d0d03b4fa4189f4f341bc3f703f4e99";
+  url = https://github.com/vdemeester/i3-config/archive/v1.2.tar.gz;
+  sha256 = "1a097db217620dff4b0e1eb965e885cc0fca9bcff9564bf2d6f99e7ac3196212";
   name = "sbr-i3-config-${version}";
   version = "1.1";
 }
