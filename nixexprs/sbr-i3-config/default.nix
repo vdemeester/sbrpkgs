661768efc74d98c8d1b897ba734cbb662bff8722
Vincent Demeester <vincent@sbr.pm>
2016-09-23 20:17:37 +0200

    Forgot to update the version..
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-i3-config/default.nix
+++ nixexprs/sbr-i3-config/default.nix
@@ -4,5 +4,5 @@ sbr-config rec {
   url = https://github.com/vdemeester/i3-config/archive/v1.2.tar.gz;
   sha256 = "1a097db217620dff4b0e1eb965e885cc0fca9bcff9564bf2d6f99e7ac3196212";
   name = "sbr-i3-config-${version}";
-  version = "1.1";
+  version = "1.2";
 }
