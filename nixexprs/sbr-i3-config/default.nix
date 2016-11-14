59148f20e9e97800fc2a87f91e4ccad746ad5dfc
Vincent Demeester <vincent@sbr.pm>
2016-11-14 18:47:06 +0100

    Update i3-config

--- nixexprs/sbr-i3-config/default.nix
+++ nixexprs/sbr-i3-config/default.nix
@@ -1,8 +1,8 @@
 { stdenv, system ? builtins.currentSystem, sbr-config }:
 
 sbr-config rec {
-  url = https://github.com/vdemeester/i3-config/archive/v1.3.tar.gz;
-  sha256 = "11yjd9vizh682bs5dbl0qkm3pmvm02l91myb4r9z0xng1p3ai0ls";
+  url = https://github.com/vdemeester/i3-config/archive/v1.4.tar.gz;
+  sha256 = "12qy8m9jmfk4n7skcbc26xz8qps9q65wlzlhlwl0fyfxv907bpdr";
   name = "sbr-i3-config-${version}";
-  version = "1.3";
+  version = "1.4";
 }
