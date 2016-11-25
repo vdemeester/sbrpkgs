348f3c8ee1df39043ae7230d7a7118976a28ca1d
Vincent Demeester <vincent@sbr.pm>
2016-11-25 23:46:37 +0100

    Update sbr-sh-config to v1.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-sh-config/default.nix
+++ nixexprs/sbr-sh-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/sh-config/archive/v${version}.tar.gz";
-  sha256 = "1am692ajn93m0kqaa45xdl36q8mmgsvgmvf81n14hbzbik7h50zr";
+  sha256 = "06xdk9b91vvnvd2dqd0422n28nn1k1xl4kh3y6gxb296w3wc2i0w";
   name = "sbr-sh-config";
-  version = "1.0";
+  version = "1.1";
 }
