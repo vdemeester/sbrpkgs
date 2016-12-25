d758eca518e986b49438cb3778104a91877f3682
Vincent Demeester <vincent@sbr.pm>
2016-12-25 11:16:01 +0100

    … and set a version number..
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-nix-config/default.nix
+++ nixexprs/sbr-nix-config/default.nix
@@ -1,7 +1,8 @@
 { stdenv, system ? builtins.currentSystem }:
 
 stdenv.mkDerivation rec {
-  name = "sbr-nix-config";
+  name = "sbr-nix-config-${version}";
+  version = "0.1";
   builder = ./builder.sh;
   src = ./.;
 }
