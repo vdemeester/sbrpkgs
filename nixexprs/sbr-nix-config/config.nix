1d32a7a2f8b8d59be8db45e56109ae70c03ec7b3
Vincent Demeester <vincent@sbr.pm>
2016-12-25 11:24:49 +0100

    Just a format "cleaning" kinda 👼
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-nix-config/config.nix
+++ nixexprs/sbr-nix-config/config.nix
@@ -1,22 +1,25 @@
 {
-    allowUnfree = true;
-  packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
-    all = with pkgs; buildEnv {  # pkgs is your overriden set of packages itself
-      name = "all";
-      paths = [
-	pandoc
-	dmenu2
-	jq
-	keybase
-	youtube-dl
-      ];
-    };
-    browsers = with pkgs; buildEnv {
-      name = "browsers";
-      paths = [
-        firefox
-        google-chrome
-      ];
-    };
-  };
+	allowUnfree = true; # mainly for idea-ultimate and some other stuff
+	# PackageOverrides allows you to update stuff in a bunch and when you want
+	# - http://pastebin.com/waUNDPYw
+	# - https://nixos.org/wiki/Howto_keep_multiple_packages_up_to_date_at_onec
+	packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
+		all = with pkgs; buildEnv {  # pkgs is your overriden set of packages itself
+			name = "all";
+			paths = [
+				pandoc
+				dmenu2
+				jq
+				keybase
+				youtube-dl
+			];
+		};
+		browsers = with pkgs; buildEnv {
+			name = "browsers";
+			paths = [
+				firefox
+				google-chrome
+			];
+		};
+	};
 }
