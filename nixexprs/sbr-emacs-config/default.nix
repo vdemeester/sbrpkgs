91b3767c18d1324f57b1fc078649cc13e9d0952f
Vincent Demeester <vincent@sbr.pm>
2017-01-15 23:17:43 +0100

    Update emacs-config to v0.2.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-emacs-config/default.nix
+++ nixexprs/sbr-emacs-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/emacs-config/archive/v${version}.tar.gz";
-  sha256 = "1kixnhlad5jm2hz8pdbjii9dgisa2rgx3s3kjcmbghs9awsisj4k";
+  sha256 = "1wvp4k5c67dj19f4y8lisgvq4h4nzwvr3228ryfp6v1ifc659d4m";
   name = "sbr-emacs-config";
-  version = "0.2";
+  version = "0.2.1";
 }
