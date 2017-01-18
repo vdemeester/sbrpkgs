d8ded55d3d2a0fd36fba6c1c2b2207189f228284
Vincent Demeester <vincent@sbr.pm>
2017-01-18 12:16:30 +0100

    Update git-config to v0.1.2
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/sbr-git-config/default.nix
+++ nixexprs/sbr-git-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/git-config/archive/v${version}.tar.gz";
-  sha256 = "12m5qmki3jpbmig6i3zahdv4xhn9qnlb3syp4084mds144dd1glm";
+  sha256 = "17vbhp03x5agfnp0m7mm6qhk4k866karsn19vncnyj07ppxznm2g";
   name = "sbr-git-config";
-  version = "0.1.1";
+  version = "0.1.2";
 }
