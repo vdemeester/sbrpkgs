f46e0199b307d923bb3b67e8d72c005bd625561c
Vincent Demeester <vincent@sbr.pm>
2017-01-19 13:22:44 +0100

    Remove docker from sbrpkgs
    I'm now using my branch or nixpkgs directly
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/pkgs/default.nix
+++ nixexprs/pkgs/default.nix
@@ -16,21 +16,6 @@ rec {
   };
   
   # from nixpkgs
-  docker = import ./docker {
-    docker-proxy = docker-proxy;
-    tini = tini;
-    inherit (pkgs) stdenv lib fetchFromGitHub makeWrapper pkgconfig
-    containerd runc go-md2man
-    go sqlite iproute bridge-utils devicemapper systemd
-    btrfs-progs iptables e2fsprogs xz utillinux xfsprogs
-    procps;
-  };
-  docker-proxy = import ./docker/proxy.nix {
-    inherit (pkgs) stdenv lib fetchFromGitHub go docker;
-  };
-  tini = import ./tini {
-    inherit (pkgs) stdenv fetchFromGitHub cmake glibc;
-  };
   dobi = import ./dobi {
     inherit (pkgs) stdenv lib buildGoPackage fetchFromGitHub;
   };
