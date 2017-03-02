d3a92097d5da57e180592c1449bcb9bb83d89fc3
Vincent Demeester <vincent@sbr.pm>
2017-03-02 14:11:25 +0100

    Update emacs-config to v0.3.0
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/emacs-config/default.nix
+++ nixexprs/commons/emacs-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/emacs-config/archive/v${version}.tar.gz";
-  sha256 = "1b7sr3ckxagsc0p0rdmj2qw5z53128csk6c679hw5x9xvakcxjgh";
+  sha256 = "10n80qj26sch977g9x1cmiv1k52d81i6hpv00kysr600wijk9z76";
   name = "sbr-emacs-config";
-  version = "0.2.3";
+  version = "0.3.0";
 }
