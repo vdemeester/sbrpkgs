041008a414e5608f81178df1bbec6743f53ead2f
Vincent Demeester <vincent@sbr.pm>
2017-05-13 20:13:47 +0200

    Update i3-config to 1.5.2
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/i3-config/default.nix
+++ nixexprs/commons/i3-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/i3-config/archive/v${version}.tar.gz";
-  sha256 = "1cvcc3frrvr0795c0glbm80nv7g4pk67r7iixsrb60jmgn0cqzk6";
+  sha256 = "01280611a2nh11vdhhy7c3hbs1gaznr18m2j4qb9fpk9dr47l2as";
   name = "sbr-i3-config";
-  version = "1.5.1";
+  version = "1.5.2";
 }
