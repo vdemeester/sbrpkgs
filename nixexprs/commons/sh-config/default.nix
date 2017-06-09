fc4eeb67d3c632e1f59b7f0278aa9a1dc879e4bf
Vincent Demeester <vincent@sbr.pm>
2017-06-09 22:03:01 +0200

    Update sh-config to v1.4
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/sh-config/default.nix
+++ nixexprs/commons/sh-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/sh-config/archive/v${version}.tar.gz";
-  sha256 = "0m22gnr9qnimk099vpccykmygajxmcr6wrh9f3lw57argzvc9106";
+  sha256 = "0ga0nmxch6j1x00r6djaqj8aarx014ag9jzj2r6q4xci34qjs38p";
   name = "sbr-sh-config";
-  version = "1.3";
+  version = "1.4";
 }
