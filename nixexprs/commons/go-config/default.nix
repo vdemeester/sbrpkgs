2072b1930c43d6cd9c807c286920590a94447edc
Vincent Demeester <vincent@sbr.pm>
2017-06-18 14:16:35 +0200

    Update go-config to v0.3.5
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/go-config/default.nix
+++ nixexprs/commons/go-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/go-config/archive/v${version}.tar.gz";
-  sha256 = "0rjbirxsa4b6973s32sjy62bjkzffkfkvp0jyhjpnxi3gxlbhml0";
+  sha256 = "0rjfpba3bh5rclfkc51ad12cad72p0bbp8lw7vj703w7vzb3lpik";
   name = "sbr-go-config";
-  version = "0.3.4";
+  version = "0.3.5";
 }
