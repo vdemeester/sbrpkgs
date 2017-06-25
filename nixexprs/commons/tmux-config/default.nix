cab07d3d8aacf48faeeb47dc80dfe83230be39f4
Vincent Demeester <vincent@sbr.pm>
2017-06-25 12:09:11 +0200

    Update tmux-config to v1.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/tmux-config/default.nix
+++ nixexprs/commons/tmux-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/tmux-config/archive/v${version}.tar.gz";
-  sha256 = "0xdbc9ghgbfkgmmlqhmj7pxkvmqj2jm28cpx3d64hlpwjvnrdjcq";
+  sha256 = "14jk4cjdqr3kqb6489awlf109qx325jsq7ws8skjn3a5p2rnf7ns";
   name = "sbr-tmux-config";
-  version = "1.0";
+  version = "1.1";
 }
