4abfeb67b8cb606551dfe0bc23256f0c2ca2ca44
Vincent Demeester <vincent@sbr.pm>
2017-07-07 15:08:56 +0200

    Update go-config to 0.4.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/go-config/default.nix
+++ nixexprs/commons/go-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/go-config/archive/v${version}.tar.gz";
-  sha256 = "0rjfpba3bh5rclfkc51ad12cad72p0bbp8lw7vj703w7vzb3lpik";
+  sha256 = "1hjkmfax6qlznd3mdwp7b9g4p79c105dk5r5s8la4cj3rxbyfw51";
   name = "sbr-go-config";
-  version = "0.3.5";
+  version = "0.4.1";
 }
