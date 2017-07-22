df156cdc83e9bd256ea06a05fe4d6ccfb366c12e
Vincent Demeester <vincent@sbr.pm>
2017-07-22 13:25:19 +0200

    Update nix-config to v0.6.1
    Put the src folder in the right place !
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/nix-config/builder.sh
+++ nixexprs/commons/nix-config/builder.sh
@@ -1,12 +1,14 @@
 source $stdenv/setup
 
+dest=$out/nix-home/sbr-nix-config/
+
 # nixpkgs configuration
-mkdir -p $out/nix-home/sbr-nix-config/.nixpkgs
-cp $src/config.nix $out/nix-home/sbr-nix-config/.nixpkgs/
+mkdir -p $dest/.nixpkgs
+cp $src/config.nix $dest/.nixpkgs/
 
 # go package direnv/nix-shells
 GO_PACKAGES=(vdemeester docker containous kubernetes libkermit moby linuxkit opencontainers containerd google ldez)
 for pkg in ${GO_PACKAGES[@]}; do
-    mkdir -p $out/nix-home/src/github.com/${pkg}
-    cp $src/go/{.envrc,default.nix} $out/nix-home/src/github.com/${pkg}
+    mkdir -p $dest/src/github.com/${pkg}
+    cp $src/go/{.envrc,default.nix} $dest/src/github.com/${pkg}
 done
--- nixexprs/commons/nix-config/default.nix
+++ nixexprs/commons/nix-config/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   name = "sbr-nix-config-${version}";
-  version = "0.6.0";
+  version = "0.6.1";
   builder = ./builder.sh;
   src = ./.;
 }
