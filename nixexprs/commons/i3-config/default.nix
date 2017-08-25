5d93dbec407ca07f5e0afda2991a289bf2e8c7a3
Vincent Demeester <vincent@sbr.pm>
2017-08-25 22:47:01 +0200

    Bump i3-config to 1.7.3
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/i3-config/default.nix
+++ nixexprs/commons/i3-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/i3-config/archive/v${version}.tar.gz";
-  sha256 = "008qs5gxasgz7v5wg09572a6f195l2sijfww0gx37hd6rh9i6ykw";
+  sha256 = "0xgyawkdkp55apg5hilz39cn326rc3yj6vya6ffskrrks586bk5f";
   name = "sbr-i3-config";
-  version = "1.7.2";
+  version = "1.7.3";
 }
