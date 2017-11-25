a765bf03e5b29cd123c7880a31ba639ccf397256
Vincent Demeester <vincent@sbr.pm>
2017-11-25 15:13:17 +0100

    Rename go to src and move default.nix to src
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/nix-config/builder.sh
+++ nixexprs/commons/nix-config/builder.sh
@@ -8,7 +8,7 @@ cp $src/config.nix $dest/.nixpkgs/
 
 # go package direnv/nix-shells
 mkdir -p $dest/src/github.com
-cp $src/go/{.envrc,default.nix} $dest/src/github.com/
+cp $src/src/{.envrc,default.nix} $dest/src/
 GO_PACKAGES=(vdemeester docker containous kubernetes moby linuxkit opencontainers containerd google gotestyourself)
 for pkg in ${GO_PACKAGES[@]}; do
     mkdir -p $dest/src/github.com/${pkg}
--- nixexprs/commons/nix-config/default.nix
+++ nixexprs/commons/nix-config/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   name = "sbr-nix-config-${version}";
-  version = "0.6.5";
+  version = "0.6.6";
   builder = ./builder.sh;
   src = ./.;
 }
