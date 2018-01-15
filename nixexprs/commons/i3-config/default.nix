284c402e0c7c7f27f6f3747a96b34ba2f43abc81
Vincent Demeester <vincent@sbr.pm>
2018-01-15 09:51:01 +0100

    i3-config: 1.7.5 -> 1.7.6
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/i3-config/default.nix
+++ nixexprs/commons/i3-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/i3-config/archive/v${version}.tar.gz";
-  sha256 = "005wscx2d36mirh08fif9qcrl5lrv99xwi4i6chilgv2y66adr5p";
+  sha256 = "1zkfckinmr9c4hsqi8acbcmqrfyp6ra0dmhg0rpyr48dyz5zzar1";
   name = "sbr-i3-config";
-  version = "1.7.5";
+  version = "1.7.6";
 }
