99ea2e4583349373e5d445fd39956d09de34d8aa
Vincent Demeester <vincent@sbr.pm>
2018-02-12 09:27:28 +0100

    Bump tmux-config to v1.2
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- nixexprs/commons/tmux-config/default.nix
+++ nixexprs/commons/tmux-config/default.nix
@@ -2,7 +2,7 @@
 
 sbr-config rec {
   url = "https://github.com/vdemeester/tmux-config/archive/v${version}.tar.gz";
-  sha256 = "14jk4cjdqr3kqb6489awlf109qx325jsq7ws8skjn3a5p2rnf7ns";
+  sha256 = "0srkir11r1pzv7ysn82jyvj8vszx166r6nzfnwas2d7v8vrsayhi";
   name = "sbr-tmux-config";
-  version = "1.1";
+  version = "1.2";
 }
